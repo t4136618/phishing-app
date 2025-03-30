f2fb1bf01906f6633bb6643a84b72b9bd4cedb5c
tehila.c <tehila.c@compie.co.il>
2025-03-30 15:51:21 +0300

    init app

---
+++ phishing-app-frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+      <div>
+        <Head>
+          <title>Phishing Tracker</title>
+          <meta name="description" content="Track phishing emails"/>
+        </Head>
+        <main>
+          <h1>Email Tracking System</h1>
+        </main>
+      </div>
+  );
+}
